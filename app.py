845bbd62eb66039a17c608ba6dec69144e01956f
Manoj0409 <136175520+Manoj0409@users.noreply.github.com>
2024-04-13 11:05:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello!! Your Flask application has been deployed successfully."
+    return "Helloooo!! Your Flask application has been deployed successfully."
 
 
 if __name__ == "__main__":
